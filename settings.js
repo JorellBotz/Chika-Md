3fa089b41c58a60985a511341e5c8b5e7150bab4
JorellBotz <102285708+JorellBotz@users.noreply.github.com>
2022-06-13 05:40:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['6283813550373','6283813550373','6283813550373'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['6283813550373'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '6283813550373' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-  global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'Jorell' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.botname = 'Rimuru-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.footer = 'Jorell' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
   global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-  global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+  global.mygc = 'https://chat.whatsapp.com/HqaKzzUUSir6INLRuppU2a'
   global.myweb = 'https://api-riychdwayne.herokuapp.com'
   global.region = 'indonesia'
 
   // Set Wm
-  global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.packname = '© Rimuru-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.author = 'Jorell' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
